812b30a2ea17f5427b46e7fb8ee5241aad87efc8
Peter Witham <peter@peterwitham.com>
2017-01-24 12:17:50 -0600

    Initial Commit
    The working project.

---
+++ PickerTextFieldExample/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  PickerTextFieldExample
+//
+//  Created by Peter Witham on 1/24/17.
+//  Copyright © 2017 Peter Witham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var theTextfield: UITextField!
+    
+    let myPickerData = [String](arrayLiteral: "Peter", "Jane", "Paul", "Mary", "Kevin", "Lucy")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let thePicker = UIPickerView()
+        thePicker.delegate = self
+        theTextfield.inputView = thePicker
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UIPickerView Delegation
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // TODO: Replace with data count
+        return myPickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // TODO: Replace with proper data
+        return myPickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        theTextfield.text = myPickerData[row]
+    }
+
+
+}
+
